794647ecfd48c0ddcdeaa24927a93354594cec53
hyo0508 <2018008168@hanyang.ac.kr>
2021-10-05 10:51:52 +0900

    Latest

--- project2/db_project/db/src/bpt.cc
+++ project2/db_project/db/src/bpt.cc
@@ -1,8 +1,86 @@
 #include "../include/bpt.h"
 #include "../include/file.h"
 
+/* Opens existing data file using pathname,
+ * or creates one if not existed.
+ * If success, returns the unique table id.
+ * Otherwise, returns -1.
+ */
+int64_t open_table(char* pathname) {
+    int64_t table_id = file_open_database_file(pathname);
+    return table_id;
+}
+
+/* Initializes DBMS.
+ * If success, returns 0. Otherwise, returns -1. 
+ */
+int init_db() {
+    open_table("my_db.db");
+    return 0;
+}
+
+/* Shutdowns DBMS.
+ * If success, returns 0. Otherwise, returns -1.
+ */
+int shutdown_db() {
+    file_close_database_file();
+    return 0;
+}
+
 // OUTPUT AND UTILITIES
 
+void print_page(pagenum_t page_num, page_t page) {
+    if (page_num == 0) {
+        printf("-----header information-----\n");
+        printf("free_num: %ld\n", page.free_num);
+        printf("num_pages: %ld\n", page.num_pages);
+        printf("root_num: %ld\n", page.root_num);
+        printf("\n");
+        return;
+    }
+	if (page.is_leaf) {
+        if (page.parent)
+		    printf("-----leaf information-----\n");
+        else
+            printf("-----root information-----\n");
+        printf("number: %ld\n", page_num);
+		printf("parent: %ld\n", page.parent);
+		printf("is_leaf: %d\n", page.is_leaf);
+		printf("num_keys: %d\n", page.num_keys);
+		printf("free_space: %ld\n", page.free_space);
+		printf("sibling: %ld\n", page.sibling);
+
+		for (int i = 0; i < page.num_keys; i++) {
+			printf("key: %3ld, size: %3d, offset: %4d, value: %s\n", page.slots[i].key, page.slots[i].size, page.slots[i].offset, page.values + page.slots[i].offset - HEADER_SIZE);
+		}
+	}
+	else {
+        if (page.parent)
+		    printf("-----page information-----\n");
+        else
+            printf("-----root information-----\n");
+        printf("number: %ld\n", page_num);
+		printf("parent: %ld\n", page.parent);
+		printf("is_leaf: %d\n", page.is_leaf);
+		printf("num_keys: %d\n", page.num_keys);
+
+		printf("left_child: %ld\n", page.left_child);
+		for (int i = 0; i < page.num_keys; i++) {
+			printf("key: %3ld, child: %ld\n", page.entries[i].key, page.entries[i].child);
+		}
+	}
+    printf("\n");
+}
+
+void print_pgnum(int64_t table_id, pagenum_t page_num) {
+	page_t page;
+	file_read_page(table_id, page_num, &page);
+	print_page(page_num, page);
+}
+
+/* Traces the path from the root to a leaf, searching by key.
+ * Returns a page # of the leaf containg the given key.
+*/
 pagenum_t find_leaf(int64_t table_id, int64_t key) {
     int i;
     pagenum_t temp_pgnum;
@@ -24,6 +102,10 @@ pagenum_t find_leaf(int64_t table_id, int64_t key) {
     return temp_pgnum;
 }
 
+/* Finds the record containing a key.
+ * Stores value & size corresponding to the key.
+ * If success, returns 0. Otherwise, returns -1.
+ */
 int db_find(int64_t table_id, int64_t key, char* ret_val, uint16_t* val_size) {
     pagenum_t page_num;
     page_t page;
@@ -45,6 +127,9 @@ int db_find(int64_t table_id, int64_t key, char* ret_val, uint16_t* val_size) {
 
 // INSERTION
 
+/* Creates a new general page, which can be adapted
+ * to serve as either a leaf or an internal page.
+ */
 pagenum_t make_page(int64_t table_id) {
     pagenum_t new_pgnum;
     page_t new_page;
@@ -57,6 +142,9 @@ pagenum_t make_page(int64_t table_id) {
     return new_pgnum;
 }
 
+/* Creates a new leaf by creating a page
+ * and then adpapting it appropriately.
+ */
 pagenum_t make_leaf(int64_t table_id) {
     pagenum_t leaf_pgnum;
     page_t leaf_page;
@@ -69,6 +157,10 @@ pagenum_t make_leaf(int64_t table_id) {
     return leaf_pgnum;
 }
 
+/* Hepler function used in insert_into_parent
+ * to find the index of the parent's child to
+ * the page to the left of the key to be inserted.
+ */
 int get_left_index(int64_t table_id, pagenum_t parent_pgnum, pagenum_t left_pgnum) {
     int left_index;
     page_t parent;
@@ -84,6 +176,8 @@ int get_left_index(int64_t table_id, pagenum_t parent_pgnum, pagenum_t left_pgnu
     return left_index;
 }
 
+/* Inserts new key & value into a leaf.
+ */
 void insert_into_leaf(int64_t table_id, pagenum_t leaf_pgnum,
                       int64_t key, char* value, uint16_t val_size) {
     page_t leaf;
@@ -116,6 +210,9 @@ void insert_into_leaf(int64_t table_id, pagenum_t leaf_pgnum,
     file_write_page(table_id, leaf_pgnum, &leaf);
 }
 
+/* Inserts new key & value into a leaf so as to exceed
+ * the page's size, causing the leaf to be split in half.
+ */
 void insert_into_leaf_after_splitting(int64_t table_id, pagenum_t leaf_pgnum,
                                       int64_t key, char* value, uint16_t val_size) {
     pagenum_t new_pgnum;
@@ -207,6 +304,8 @@ void insert_into_leaf_after_splitting(int64_t table_id, pagenum_t leaf_pgnum,
     insert_into_parent(table_id, leaf_pgnum, new_key, new_pgnum);
 }
 
+/* Insert new key & child into a page.
+ */
 void insert_into_page(int64_t table_id, pagenum_t parent_pgnum, int left_index,
                       int64_t key, pagenum_t right_pgnum) {
     page_t parent, right;
@@ -226,6 +325,9 @@ void insert_into_page(int64_t table_id, pagenum_t parent_pgnum, int left_index,
     file_write_page(table_id, parent_pgnum, &parent);
 }
 
+/* Inserts new key & child into a page so as to exceed
+ * the entry order, causing the page to be split in half.
+ */
 void insert_into_page_after_splitting(int64_t table_id, pagenum_t old_pgnum, int left_index,
                                       int64_t key, pagenum_t right_pgnum) {
     pagenum_t new_pgnum;
@@ -236,6 +338,14 @@ void insert_into_page_after_splitting(int64_t table_id, pagenum_t old_pgnum, int
     
     file_read_page(table_id, old_pgnum, &old_page);
     file_read_page(table_id, right_pgnum, &right);
+
+    /* First create a temporary set of keys & childs
+     * to hold everything in order, including
+     * the new key & child, inserted in right places.
+     * Then create a new page and copy half of the
+     * keys & childs to the old page and
+     * the other half to the new page.
+    */
 
     // need to cleanup
     for (i = 0, j = 0; i < old_page.num_keys + 1; i++, j++) {
@@ -249,6 +359,11 @@ void insert_into_page_after_splitting(int64_t table_id, pagenum_t old_pgnum, int
     }
     temp[left_index].child = right_pgnum;
     temp[left_index].key = key;
+
+    /* Create the new page and copy
+     * half the keys & childs to the
+     * old and half to the new.
+     */
 
     split = ENTRY_ORDER / 2 + 1;
     new_pgnum = make_page(table_id);
@@ -288,9 +403,16 @@ void insert_into_page_after_splitting(int64_t table_id, pagenum_t old_pgnum, int
     file_write_page(table_id, new_pgnum, &new_page); // for문 위로?
     file_write_page(table_id, old_pgnum, &old_page);
 
+    /* Insert a new key into the parent of the two
+     * pages resulting from the split, with
+     * the old node to the left and the new to the right.
+     */
+
     insert_into_parent(table_id, old_pgnum, k_prime, new_pgnum);
 }
 
+/* Inserts a new page (leaf or internal) into data file.
+ */
 void insert_into_parent(int64_t table_id,
                         pagenum_t left_pgnum, int64_t key, pagenum_t right_pgnum) {
     pagenum_t parent_pgnum;
@@ -304,23 +426,36 @@ void insert_into_parent(int64_t table_id,
 
     file_read_page(table_id, parent_pgnum, &parent);
 
+    /* Case: new root. */
     if (parent_pgnum == 0) {
         insert_into_new_root(table_id, left_pgnum, key, right_pgnum);
         return;
     }
 
+    /* Case: leaf or page.(Rest of function body)
+     */
+
+    /* Find the index of parent's child to the left page.
+     */
     left_index = get_left_index(table_id, parent_pgnum, left_pgnum);
     
+    /* Simple case: the new key fits into the node.
+     */
     if (parent.num_keys < ENTRY_ORDER - 1) {
         insert_into_page(table_id, parent_pgnum, left_index, key, right_pgnum);
     }
 
+    /* Harder case: split a node by insertion rule.
+     */
     else {
         insert_into_page_after_splitting(table_id, parent_pgnum, left_index, key, right_pgnum);
     }
     
 }
 
+/* Creates a new root for two subtrees
+ * and inserts the appropriate key into the new root.
+ */
 void insert_into_new_root(int64_t table_id,
                           pagenum_t left_pgnum, int64_t key, pagenum_t right_pgnum) {
     pagenum_t root_pgnum;
@@ -348,6 +483,8 @@ void insert_into_new_root(int64_t table_id,
     file_write_page(table_id, 0, &header);
 }
 
+/* Starts a new tree.
+ */
 void start_new_tree(int64_t table_id, int64_t key, char* value, uint16_t val_size) {
     pagenum_t root_pgnum;
     page_t root, header;
@@ -370,6 +507,10 @@ void start_new_tree(int64_t table_id, int64_t key, char* value, uint16_t val_siz
     file_write_page(table_id, 0, &header);
 }
 
+/* Master insertion function.
+ * Inserts key & value into data file at the right place.
+ * If success, returns 0. Otherwise, returns -1.
+ */
 int db_insert(int64_t table_id, int64_t key, char* value, uint16_t val_size) {
     pagenum_t leaf_pgnum;
     page_t leaf, header;
@@ -378,19 +519,32 @@ int db_insert(int64_t table_id, int64_t key, char* value, uint16_t val_size) {
 
     file_read_page(table_id, 0, &header);
 
+    /* Case: the tree does not exist yet.
+     * Start a new tree.
+     */
     if (header.root_num == 0) {
         start_new_tree(table_id, key, value, val_size);
         return 0;
     }
 
+    /* Case: the tree already exists.
+     * (Rest of function body)
+     */
+
     leaf_pgnum = find_leaf(table_id, key);
 
     file_read_page(table_id, leaf_pgnum, &leaf);
 
+    /* Case: enough free space to insert.
+     * Nothing to do.
+     */
     if (leaf.free_space >= SLOT_SIZE + val_size) {
         insert_into_leaf(table_id, leaf_pgnum, key, value, val_size);
     }
 
+    /* Case: no room for the new record.
+     * leaf must be split.
+     */
     else {
         insert_into_leaf_after_splitting(table_id, leaf_pgnum, key, value, val_size);
     }
