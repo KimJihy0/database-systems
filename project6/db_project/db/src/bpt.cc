0de6fd8554047b2f71b11d7b36a73d707ffbf08b
hyo0508 <2018008168@hanyang.ac.kr>
2021-12-13 15:54:23 +0900

    -f

--- project6/db_project/db/src/bpt.cc
+++ project6/db_project/db/src/bpt.cc
@@ -47,7 +47,7 @@ int db_find(int64_t table_id, int64_t key,
     }
     if (lock_acquire(table_id, p_pgnum, i, trx_id, SHARED, &p) != 0) {
         buffer_unpin_page(table_id, p_pgnum);
-        trx_commit(trx_id);
+        trx_abort(trx_id);
         return trx_id;
     }
 
@@ -84,7 +84,7 @@ int db_update(int64_t table_id, int64_t key,
     }
     if (lock_acquire(table_id, p_pgnum, i, trx_id, EXCLUSIVE, &p) != 0) {
         buffer_unpin_page(table_id, p_pgnum);   
-        trx_commit(trx_id);
+        trx_abort(trx_id);
         return trx_id;
     }
 
