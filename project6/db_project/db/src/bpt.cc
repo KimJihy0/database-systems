1eb4b46543e36eafec440e3c86925c1bef8fec44
hyo0508 <2018008168@hanyang.ac.kr>
2021-12-17 17:30:27 +0900

    -f

--- project6/db_project/db/src/bpt.cc
+++ project6/db_project/db/src/bpt.cc
@@ -100,6 +100,8 @@ int db_update(int64_t table_id, int64_t key,
                                      table_id, p_pgnum, offset, size, (char*)p + offset, value);
     trx_set_last_LSN(trx_id, ret_LSN);
 
+    raise(6);
+
     memcpy((char*)p + offset, value, new_val_size);
     p->page_LSN = ret_LSN;
     buffer_write_page(table_id, p_pgnum);
