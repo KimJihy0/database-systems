ea73ff15d92d45d5178872a317eae7d0073429f0
hyo0508 <2018008168@hanyang.ac.kr>
2021-12-18 22:23:14 +0900

    -f

--- project6/db_project/db/src/recov.cc
+++ project6/db_project/db/src/recov.cc
@@ -18,7 +18,7 @@ void recovery(int flag, int log_num, char* logmsg_path) {
         crash = redo_pass(fp, flag == 1 ? log_num : -1);
     }
     if (!crash) {
-        // crash = undo_pass(fp, flag == 2 ? log_num : -1);
+        crash = undo_pass(fp, flag == 2 ? log_num : -1);
     }
     fclose(fp);
 
@@ -51,7 +51,7 @@ void anls_pass(FILE* fp) {
     }
     free(anls_log);
     for (const auto& loser : losers) {
-        trx_resurrect_entry(loser);
+        // trx_resurrect_entry(loser);
     }
     fprintf(fp, "[ANALYSIS] Analysis pass end");
     fprintf(fp, ". Winner:");
